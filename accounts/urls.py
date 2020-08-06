719a3fb99d3a2c1e9bfe6063886a963e135e45b6
sexybrainpyo <pyose95@naver.com>
2020-08-06 20:48:58 +0900

    login cookie insert

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('signin', views.signin, name="signin"),
     path('signin_insert', views.signin_insert, name="signin_insert"),
     path('login_form', views.login_form, name="login_form"),
+    path('login', views.login, name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,10 +5,8 @@ from accounts.models import Account
 def signin(request):
     return render(request, "signin.html")
 
-
 def login_form(request):
     return render(request, "login_form.html")
-
 
 def signin_insert(request):
     signin_id = request.POST["signin_id"]
@@ -19,6 +17,17 @@ def signin_insert(request):
         return render(request, "signin.html", {"message_pw": "INCORRECT PASSWORD"})
     if Account.objects.filter(signin_id=signin_id).exists():
         return render(request, "signin.html", {"message_id": "EXISTING ID"}, status=401)
-    Account.objects.create(signin_id=signin_id, signin_pw=signin_pw)
 
-    return redirect('/board')
+    Account.objects.create(signin_id=signin_id, signin_pw=signin_pw)
+    return redirect('/')
+
+def login(request):
+    signin_id = request.POST["signin_id"]
+    signin_pw = request.POST["signin_pw"]
+    if Account.objects.filter(signin_id=signin_id, signin_pw=signin_pw).exists():
+        response = redirect('/')
+        response.set_cookie('signin_id', signin_id)
+        response.set_cookie('signin_pw', signin_pw)
+        return response
+    else:
+        return redirect("login_from.html")
